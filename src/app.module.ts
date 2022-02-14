71dc29237938a9cf479d2ba08bef05a49d5109ab
Ismail <forwork.ismail@gmail.com>
2022-02-14 13:41:33 +0430

    Add typeorm mongodb module to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchema: true,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson]
     }),
-  ],
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: true,
+      sortSchema: true
+    }),
+    LessonModule
+  ]
 })
 export class AppModule {}
